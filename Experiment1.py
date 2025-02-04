e9de6dd2146c1d790034efcf176a2cde600fce93
yash-flix <yash.tushar13@gmail.com>
2025-02-04 14:50:32 +0530

    Update and rename experiment 1txt to Experiment1.py

---
+++ Experiment1.py
@@ -0,0 +1,25 @@
+
+message = "Hello, this is a simple Python example!"
+print(message) 
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")  
+print(fruits) 
+
+fruits.pop() 
+print(fruits) 
+print(fruits[1])  
+
+coordinates = (10, 20, 30)
+print(coordinates) 
+print(coordinates[0]) 
+
+person = {"name": "Alice", "age": 25, "city": "Wonderland"}
+print(person)
+
+print(person["name"]) 
+
+person["age"] = 26  
+person["job"] = "Adventurer"
+
+print(person)  
